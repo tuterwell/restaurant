63f308094098dc65dc2da6b4ac2b29f2992ef4ee
tuterwell <a0981812683@gmail.com>
2023-07-06 15:25:37 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.get('/restaurant', (req, res) => {
     res.render('index',{ restaurant: restaurant})
 })
 
+app.get('/restaurant/:id', (req, res) => {
+    const id = req.params.id
+    const restaurants = restaurant.find((r) => r.id.toString() === id)
+    res.render('details',{ restaurants: restaurants})
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
