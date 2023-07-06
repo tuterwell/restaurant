28d77e3b5c595379f4f956b917b2fb5799a83db7
tuterwell <a0981812683@gmail.com>
2023-07-06 15:11:02 +0800

    fear : add listing page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {engine} = require('express-handlebars')
 const app = express()
 const port = 3000
+const restaurant = require('./public/jsons/restaurant.json').results
 
 app.engine('.hbs', engine({extname : '.hbs'}))
 app.set('view engine', '.hbs')
@@ -14,7 +15,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurant', (req, res) => {
-    res.render('index')
+    res.render('index',{ restaurant: restaurant})
 })
 
 app.listen(port, () => {
