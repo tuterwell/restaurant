589019e02cb7e5943e29288aeea15a6dd0ebca61
tuterwell <a0981812683@gmail.com>
2023-07-06 14:06:23 +0800

    build: add static files & json for model

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
+    res.redirect('/restaurant')
+})
+
+app.get('/restaurant', (req, res) => {
     res.send('Hello World!')
 })
 
