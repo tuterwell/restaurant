29e5ac7e58b64dfe42e2fdea8c1fa3f5a5de9348
tuterwell <a0981812683@gmail.com>
2023-07-17 14:08:56 +0800

    router init

--- app.js
+++ app.js
@@ -17,21 +17,41 @@ app.get('/', (req, res) => {
 app.get('/restaurant', (req, res) => {
     const keyword = req.query.keyword?.trim()
     const matchRestaurant = keyword ? restaurant.filter((r) => 
-    Object.values(r).some((v) =>{
-        if (typeof v === 'string'){
-            return v.toLowerCase().includes(keyword.toLowerCase())
+    Object.values(r).some((candidate) =>{
+        if (typeof candidate === 'string'){
+            return candidate.toLowerCase().includes(keyword.toLowerCase())
         }
         return false
     }) ): restaurant
     res.render('index',{ restaurant: matchRestaurant, keyword: keyword })
-})
+}) // get all restaurants
+
+app.get('/restaurant/new', (req, res) => {
+    res.render('new')
+}) // create todo
+
+app.post('/restaurant', (req, res) => {
+    res.redirect('/restaurant')
+}) // add todo
 
 app.get('/restaurant/:id', (req, res) => {
     const id = req.params.id
     const restaurants = restaurant.find((r) => r.id.toString() === id)
     res.render('details',{ restaurants: restaurants})
-})
+}) // get single restaurant
+
+app.get('/restaurant/:id/edit', (req, res) => {
+    res.send('edit')
+}) // edit restaurant
+
+app.put('/restaurant/:id', (req, res) => {
+    res.send('update')
+}) // modify restaurant
+
+app.delete('/restaurant/:id', (req, res) => {
+    res.send('delete')
+}) // delete restaurant
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(` Server is listening on port ${port}`)
 })
