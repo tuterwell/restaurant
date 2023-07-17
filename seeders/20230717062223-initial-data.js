9e33cd6200bfef88fb6f7abd8e69a0cccc93a199
tuterwell <a0981812683@gmail.com>
2023-07-17 15:03:52 +0800

    add restaurant seeder

---
+++ seeders/20230717062223-initial-data.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    let restaurant = require('C:\\Users\\a0981\\restaurant\\public\\jsons\\restaurant.json').results
+    await queryInterface.bulkInsert('restaurants', restaurant, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('restaurants', null, {})
+  }
+};
